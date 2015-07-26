46df78b28febbcb4e2139aa3d2ca15d1764b6187
Travis Meyer <meyert11@gmail.com>
2015-07-26 14:17:40 -0400

    Assignment 2
    This is Programming assignment 2 copied to my newly created repository

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+## These functions create a matrix object, and if its inverse is not cached from
+## previous runs, it generates the inverse and caches it.
+
+## makeCacheMatrix brings in the variabel x, creates its inverse and caches it.
+
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+}
+
+
+## This function reads in a matrix and checks whether a cache of its inverse
+## exists, if not it creates it.
+
+cacheSolve <- function(x, ...) {
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
+}
